c515701a6443aa050b3229e04cd24d346db0f0d9
SamSaysHecc <80711125+Sameony@users.noreply.github.com>
2021-09-26 12:58:54 +0000

    feat: Leap year

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ if(flag===0)
   console.log("It is a prime number");
 else
   console.log("It is not a prime");
+
+//Leap Year
+const inp2 = readLineSync.question("Enter an Year: ");
+function isLeapYear(year) {
+    if (year % 400 === 0)
+        return true;
+    if (year % 100 === 0)
+        return false;
+    if (year % 4 === 0)
+        return true;
+    return false;
+}
+if(isLeapYear(inp2))
+{
+  console.log("It is a leap year");
+}
+else {
+  console.log("It is not a leap year");
+}
